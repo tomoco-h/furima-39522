d7051f5dc4e21c1b4238cce5ae6bb5e54ad995d4
tomoco-h <tmc.hrtk.10480216@gmail.com>
2023-10-21 16:34:15 +0900

    Basic認証の導入（修正版）

---
+++ test/controllers/furima_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class FurimaControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get furima_index_url
+    assert_response :success
+  end
+end
