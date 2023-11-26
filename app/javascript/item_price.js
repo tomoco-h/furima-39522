b92d3c4dc8a25781d7422aa83e4a7a97e8ad0d32
tomoco-h <tmc.hrtk.10480216@gmail.com>
2023-11-26 14:12:33 +0900

    価格のコンマ、小数点切り捨ての実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,12 +4,13 @@ function calc (){
   itemPrice.addEventListener('input', function(){
     const itemPriceValue = itemPrice.value
     const Tax = 0.1;
-    const tax_fee = itemPriceValue * Tax;
+    const tax_fee = Math.floor(itemPriceValue * Tax);
     const profit = itemPriceValue - tax_fee;
     const taxArea = document.getElementById("add-tax-price");
     const profitArea = document.getElementById("profit");
-    taxArea.innerHTML = tax_fee;
-    profitArea.innerHTML = profit;
+    taxArea.innerHTML = tax_fee.toLocaleString();
+    profitArea.innerHTML = profit.toLocaleString();
+
   })
 }
 
