2267bff9a9923124f670e4477d4d8e8cda6f8663
tomoco-h <tmc.hrtk.10480216@gmail.com>
2023-11-26 13:37:37 +0900

    JavaScriptの実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+function calc (){
+  const itemPrice = document.getElementById("item-price");
+
+  itemPrice.addEventListener('input', function(){
+    const itemPriceValue = itemPrice.value
+    const Tax = 0.1;
+    const tax_fee = itemPriceValue * Tax;
+    const profit = itemPriceValue - tax_fee;
+    const taxArea = document.getElementById("add-tax-price");
+    const profitArea = document.getElementById("profit");
+    taxArea.innerHTML = tax_fee;
+    profitArea.innerHTML = profit;
+  })
+}
+
+window.addEventListener('load', calc)
