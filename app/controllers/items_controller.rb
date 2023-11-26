358dd3c2046706ca627089d599c63af76c2f55ba
tomoco-h <tmc.hrtk.10480216@gmail.com>
2023-11-27 00:28:42 +0900

    商品情報編集機能の実装_ご指摘箇所の修正v1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :seller, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,30 +22,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-
-    unless current_user && current_user.id == @item.user_id
-      redirect_to root_path, alert: "編集権限がありません"
-    end
-
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(items_params)
       redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
-    end
-
-    unless current_user && current_user.id == @item.user_id
-      redirect_to root_path, alert: "編集権限がありません"
-      return
     end
   end
 
@@ -52,4 +40,16 @@ class ItemsController < ApplicationController
   def items_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :condition_id, :charge_id, :prefecture_id, :shipping_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def seller
+    unless current_user && current_user.id == @item.user_id
+      redirect_to root_path, alert: "編集権限がありません"
+      return
+    end
+  end
+
 end
